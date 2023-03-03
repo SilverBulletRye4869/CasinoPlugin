b37dd5ea87ec56786cb1529099ca8e98d012935c
SilverBullet_Rye <silverbulletrye4869@gmail.com>
2023-03-03 16:50:17 +0900

    VaultAPI対応

--- src/main/java/silverassist/casinoplugin/CasinoPlugin.java
+++ src/main/java/silverassist/casinoplugin/CasinoPlugin.java
@@ -7,17 +7,25 @@ import java.util.logging.Logger;
 public final class CasinoPlugin extends JavaPlugin {
     private static JavaPlugin plugin = null;
     private static Logger log = null;
+    private static Vault vault = null;
 
     @Override
     public void onEnable() {
         plugin = this;
         log = this.getLogger();
+
+        vault = new Vault(this);
+        if (!vault.setupEconomy() ) vault.log.severe(String.format("[%s] プラグイン「Vault」「Essentials」の認証に失敗しました。", getDescription().getName()));
+
+
+
         // Plugin startup logic
 
     }
 
     public static JavaPlugin getInstance(){return plugin;}
     public static Logger getLog(){return log;}
+    public static Vault getVault(){return vault;}
 
     @Override
     public void onDisable() {
