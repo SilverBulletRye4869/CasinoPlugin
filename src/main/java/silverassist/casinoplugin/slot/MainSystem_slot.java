dc0590d67003860ddc7139d92cb99cf4d82e0290
SilverBullet_Rye <silverbulletrye4869@gmail.com>
2023-03-03 16:48:52 +0900

    額縁設定棒の取得部追加

--- src/main/java/silverassist/casinoplugin/slot/MainSystem_slot.java
+++ src/main/java/silverassist/casinoplugin/slot/MainSystem_slot.java
@@ -1,13 +1,20 @@
 package silverassist.casinoplugin.slot;
 
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 import silverassist.casinoplugin.CustomConfig;
+import silverassist.casinoplugin.Util;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 
 public class MainSystem_slot {
+    public final Function<String,ItemStack> FRAME_SET_STICK = id -> Util.createItem(Material.STICK,"§6§l額縁編集棒", List.of("§fid: "+id,"§e§l額縁に対して右クリック！"),Map.of(Enchantment.DAMAGE_ALL,1));
     private final JavaPlugin plugin;
     private final Map<String,Spin> SLOTS = new HashMap<>();
 
