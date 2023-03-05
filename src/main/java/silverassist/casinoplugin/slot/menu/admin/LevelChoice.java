e54b9581b693a15eebf8e69595098eeeacf78cf6
SilverBullet_Rye <silverbulletrye4869@gmail.com>
2023-03-05 19:34:33 +0900

    スロットの変更対象レベル選択

--- src/main/java/silverassist/casinoplugin/slot/menu/admin/LevelChoice.java
+++ src/main/java/silverassist/casinoplugin/slot/menu/admin/LevelChoice.java
@@ -1,5 +1,7 @@
 package silverassist.casinoplugin.slot.menu.admin;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.HandlerList;
@@ -7,7 +9,9 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.plugin.java.JavaPlugin;
+import silverassist.casinoplugin.Util;
 import silverassist.casinoplugin.slot.MainSystem_slot;
 
 public class LevelChoice {
@@ -25,21 +29,26 @@ public class LevelChoice {
     }
 
     public void open(){
-
+        Inventory inv = Bukkit.createInventory(P,54,"§d§lレベル選択");
+        Util.invFill(inv);
+        for(int i=0;i<7;i++)inv.setItem(10+i,Util.createItem(Material.DROPPER,"§6§lレベル: "+(i+1)));
+        Util.delayInvOpen(P,inv);
     }
 
     private class listener implements Listener {
         @EventHandler
         public void onInvenotryClick(InventoryClickEvent e){
             if(!P.equals(e.getWhoClicked()) || e.getCurrentItem() == null || !e.getClickedInventory().getType().equals(InventoryType.CHEST))return;
-
+            e.setCancelled(true);
+            int slot = e.getSlot();
+            if(slot>9 && slot<17)new CategoryChoice(plugin,MAIN_SYSTEM,P,ID,slot-10);
         }
 
         @EventHandler
         public void onInventoryClose(InventoryCloseEvent e){
             if(!P.equals(e.getPlayer()))return;
-
             HandlerList.unregisterAll(this);
+            new MainMenu(plugin,MAIN_SYSTEM,P,ID).open();
         }
     }
 }
