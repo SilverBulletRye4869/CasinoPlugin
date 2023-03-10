111d7337df5e8df4653e812d80790812d2b1d210
SilverBullet_Rye <silverbulletrye4869@gmail.com>
2023-03-10 18:21:11 +0900

    額縁の登録が指定数以上にならないように修正

--- src/main/java/silverassist/casinoplugin/slot/ItemFrameRegister.java
+++ src/main/java/silverassist/casinoplugin/slot/ItemFrameRegister.java
@@ -2,18 +2,20 @@ package silverassist.casinoplugin.slot;
 
 import org.bukkit.Material;
 import org.bukkit.entity.ItemFrame;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
-import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 import silverassist.casinoplugin.CustomConfig;
 
+import java.util.List;
+
 public class ItemFrameRegister implements Listener {
+    public static final int ITEM_FRAME_COUNT = 3;
+
     private final JavaPlugin plugin;
 
     public ItemFrameRegister(JavaPlugin plugin){
@@ -31,7 +33,10 @@ public class ItemFrameRegister implements Listener {
         ItemMeta meta;
         if(!item.hasItemMeta() || !(meta = item.getItemMeta()).hasLore())return;
         String id = meta.getLore().get(0).substring(6);
-        CustomConfig.getYmlByID(id).getStringList("itemframes").add(uuidStr);
+        List<String> itemframes = CustomConfig.getYmlByID(id).getStringList("itemframes");
+        itemframes.add(uuidStr);
+        if(itemframes.size()>ITEM_FRAME_COUNT)itemframes.subList(itemframes.size()-ITEM_FRAME_COUNT,itemframes.size());
+        CustomConfig.getYmlByID(id).set("itemframes",itemframes);
         CustomConfig.saveYmlByID(id);
     }
 
