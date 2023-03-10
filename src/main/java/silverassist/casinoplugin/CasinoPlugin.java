ce23d3942bd97f20f48890c2db4194fc69d2125e
SilverBullet_Rye <silverbulletrye4869@gmail.com>
2023-03-10 18:23:24 +0900

    folderSetupを追加

--- src/main/java/silverassist/casinoplugin/CasinoPlugin.java
+++ src/main/java/silverassist/casinoplugin/CasinoPlugin.java
@@ -4,6 +4,11 @@ import org.bukkit.plugin.java.JavaPlugin;
 import silverassist.casinoplugin.slot.MainSystem_slot;
 import silverassist.casinoplugin.slot.SlotCommand;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.logging.Logger;
 
 public final class CasinoPlugin extends JavaPlugin {
@@ -17,11 +22,19 @@ public final class CasinoPlugin extends JavaPlugin {
         plugin = this;
         log = this.getLogger();
 
+        if(!folderSetup()){
+            Util.sendConsole("プラグインフォルダの作成に失敗しました", Util.MessageType.ERROR);
+            this.getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+
         vault = new Vault(this);
         if (!vault.setupEconomy() ) vault.log.severe(String.format("[%s] プラグイン「Vault」「Essentials」の認証に失敗しました。", getDescription().getName()));
 
         SLOT_MAIN_SYSTM = new MainSystem_slot(this);
         new SlotCommand(this,SLOT_MAIN_SYSTM);
+
+        new Test(this);
 
         // Plugin startup logic
 
@@ -34,5 +47,50 @@ public final class CasinoPlugin extends JavaPlugin {
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    private boolean folderSetup(){
+        try{
+            Files.createDirectories(Paths.get(this.getDataFolder()+"/data"));
+            Files.createDirectories(Paths.get(this.getDataFolder()+"/log"));
+        }catch (IOException e){
+            Util.sendConsole("dataフォルダ又はlogフォルダの作成に失敗しました", Util.MessageType.ERROR);
+            e.printStackTrace();
+            return false;
+        }
+        File file = new File(this.getDataFolder()+"/README.txt");
+
+        try {
+            file.createNewFile();
+        }catch (IOException e){
+            if(Files.exists(Paths.get(this.getDataFolder()+"README.txt")))return true;
+            else{
+                Util.sendConsole("README.txtファイルの作成に失敗しました", Util.MessageType.ERROR);
+                e.printStackTrace();
+                return false;
+            }
+        }
+        if(!file.isFile()){
+            Util.sendConsole("README.txtはファイルではありません",Util.MessageType.ERROR);
+            return false;
+        }
+        if(!file.canWrite()){
+            Util.sendConsole("README.txtは書き込み不可のファイルです",Util.MessageType.ERROR);
+            return false;
+        }
+
+        try {
+            FileWriter fw = new FileWriter(file);
+            fw.write(
+                    "[注意] config.yml以外のファイルを手動で編集した場合の動作は保証しません\n" +
+                            "[Note] We do not guarantee the operation if files other than \"config.yml\" are edited manually."
+            );
+            fw.close();
+        }catch (IOException e){
+            Util.sendConsole("README.txtファイルの書き込みに失敗しました",Util.MessageType.ERROR);
+            return false;
+        }
+
+        return true;
     }
 }
