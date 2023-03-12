3d21e809be9750a878966749b4a35c2112f3dd29
SilverBullet_Rye <silverbulletrye4869@gmail.com>
2023-03-12 18:58:55 +0900

    使用するメソッド間違ってた...

--- src/main/java/silverassist/casinoplugin/slot/ItemFrameRegister.java
+++ src/main/java/silverassist/casinoplugin/slot/ItemFrameRegister.java
@@ -1,41 +1,44 @@
 package silverassist.casinoplugin.slot;
 
-import org.bukkit.Material;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.ItemFrame;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
-import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 import silverassist.casinoplugin.CustomConfig;
-
 import java.util.List;
 
 public class ItemFrameRegister implements Listener {
     public static final int ITEM_FRAME_COUNT = 3;
 
     private final JavaPlugin plugin;
+    private final MainSystem_slot MAIN_SYSTEM;
 
-    public ItemFrameRegister(JavaPlugin plugin){
+    public ItemFrameRegister(JavaPlugin plugin,MainSystem_slot mainSystem){
         this.plugin = plugin;
+        this.MAIN_SYSTEM = mainSystem;
         plugin.getServer().getPluginManager().registerEvents(this,plugin);
     }
 
     @EventHandler
-    public void onItemFrameClick(PlayerInteractEvent e){
-        if(!e.getAction().equals(Action.RIGHT_CLICK_BLOCK))return;
-        if(!e.getClickedBlock().getType().equals(Material.ITEM_FRAME))return;
-        ItemFrame itemFrame = (ItemFrame) e.getClickedBlock().getState();
-        String uuidStr = itemFrame.getUniqueId().toString();
+    public void onItemFrameClick(PlayerInteractEntityEvent e){
+        Entity entity = e.getRightClicked();
+        if(!e.getPlayer().isOp() || entity == null || !(entity instanceof ItemFrame))return;
+        String uuidStr = entity.getUniqueId().toString();
         ItemStack item = e.getPlayer().getInventory().getItemInMainHand();
         ItemMeta meta;
         if(!item.hasItemMeta() || !(meta = item.getItemMeta()).hasLore())return;
         String id = meta.getLore().get(0).substring(6);
+        if(!MAIN_SYSTEM.existSlot(id))return;
+        e.setCancelled(true);
         List<String> itemframes = CustomConfig.getYmlByID(id).getStringList("itemframes");
+        if(itemframes.contains(uuidStr))return;
         itemframes.add(uuidStr);
-        if(itemframes.size()>ITEM_FRAME_COUNT)itemframes.subList(itemframes.size()-ITEM_FRAME_COUNT,itemframes.size());
+        if(itemframes.size()>ITEM_FRAME_COUNT)itemframes = itemframes.subList(itemframes.size()-ITEM_FRAME_COUNT,itemframes.size());
+
         CustomConfig.getYmlByID(id).set("itemframes",itemframes);
         CustomConfig.saveYmlByID(id);
     }
