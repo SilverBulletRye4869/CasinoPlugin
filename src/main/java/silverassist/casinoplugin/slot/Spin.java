e26a25b33419b3e193457f182c8f0438e29649ac
SilverBullet_Rye <silverbulletrye4869@gmail.com>
2023-03-13 18:00:16 +0900

    看板アップデートが同期処理じゃないとつかえなかったのでしゅうせい

--- src/main/java/silverassist/casinoplugin/slot/Spin.java
+++ src/main/java/silverassist/casinoplugin/slot/Spin.java
@@ -1,21 +1,22 @@
 package silverassist.casinoplugin.slot;
 
 import org.bukkit.Bukkit;
-import org.bukkit.Location;
+import org.bukkit.block.Sign;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.ItemFrame;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 import silverassist.casinoplugin.CustomConfig;
 import silverassist.casinoplugin.Util;
 import silverassist.casinoplugin.Vault;
-
 import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 public class Spin {
     private static final int ITEM_FRAME_COUNT = ItemFrameRegister.ITEM_FRAME_COUNT;
+    private static final int SPIN_TICK = 5;
 
     private final JavaPlugin plugin;
     private final MainSystem_slot MAIN_SYSTEM;
@@ -25,7 +26,7 @@ public class Spin {
     private final List<ItemStack> DISPLAY_ITEMS = new ArrayList<>();
     private final LinkedHashMap<Integer,String> PROBABILITY = new LinkedHashMap<>();
     private final HashMap<String,ItemStack[]> CATEGORY_CONTAIN_ITEMS = new HashMap<>();
-    private final List<UUID> ITEM_FRAMES;
+    private final List<ItemFrame> ITEM_FRAMES;
     private final int[] SPIN_TIME = new int[ITEM_FRAME_COUNT];
     private final int STOCK_PER_SPIN;
     private final int PAYMENT;
@@ -35,85 +36,96 @@ public class Spin {
     private final Set<String> BROADCAST = new HashSet<>();
     private final Set<String> TITLE = new HashSet<>();
     private final HashMap<String, String> NEXT_MODE=new HashMap<>();
-    private final Location SIGN_POS;
+    private final Sign SIGN;
 
     private boolean isSpinning = false;
     private int categoryRandomMax = 0;
 
-    private String mode = "1";
+    private String mode = "-1";
     public Spin(JavaPlugin plugin, MainSystem_slot mainSystem, String id){
         this.plugin = plugin;
         this.MAIN_SYSTEM = mainSystem;
         this.ID = id;
         SYSTEM_YML = CustomConfig.getYmlByID(id);
         this.NAME = SYSTEM_YML.getString("name",ID);
-        this.mode = SYSTEM_YML.getString("nowMode","1");
-        ITEM_FRAMES = SYSTEM_YML.getStringList("itemframes").stream().map(UUID::fromString).filter(g-> Util.getItemFrame(g)!=null).collect(Collectors.toList());
-        for(int i = 0; i< ITEM_FRAME_COUNT; i++)SPIN_TIME[i] = (i==0 ? 0 : SPIN_TIME[i-1]) + SYSTEM_YML.getInt("spintime."+i);
+        String nowmode = SYSTEM_YML.getString("nowmode","0");
+        setMode(nowmode);
+        this.mode = nowmode;
+        ITEM_FRAMES = SYSTEM_YML.getStringList("itemframes").stream().map(UUID::fromString).map(g->(ItemFrame)Bukkit.getEntity(g)).collect(Collectors.toList());
+        for(int i = 0; i< ITEM_FRAME_COUNT; i++)SPIN_TIME[i] = (i==0 ? 0 : SPIN_TIME[i-1]) + SYSTEM_YML.getInt("spintime."+(i+1));
         STOCK_PER_SPIN = SYSTEM_YML.getInt("stock_per_spin",0);
         PAYMENT = SYSTEM_YML.getInt("payment",100);
-        SIGN_POS = SYSTEM_YML.getLocation("sign");
-        setMode(mode);
+        SIGN = (Sign)SYSTEM_YML.getLocation("sign").getBlock().getState();
     }
 
     public boolean run(Player p){
-        if(isSpinning || PROBABILITY.keySet().size() == 0 || ITEM_FRAMES.size() < ITEM_FRAME_COUNT)return false;
+        if(PROBABILITY.keySet().size() == 0 || ITEM_FRAMES.size() < ITEM_FRAME_COUNT)return false;
+        if(isSpinning){
+            Util.sendPrefixMessage(p,"§c§lこのスロットは現在まわっています");
+            return true;
+        }
         isSpinning = true;
-        Bukkit.getScheduler().runTaskAsynchronously(plugin,()->{
-            Vault.getEconomy().withdrawPlayer(p,PAYMENT);
-            int nowStock = getAndUpdateStock(true);
 
-            ItemStack memo = null, bingoItem = null;
-            int bingoNum = (int) (Math.random() * categoryRandomMax);
-            String category = null;
-            for(int i : PROBABILITY.keySet()){
-                if(bingoNum < i){
-                    category=PROBABILITY.get(i);
-                    if(!category.equals("miss"))bingoItem=(ItemStack) Util.getRandomIndex(CATEGORY_CONTAIN_ITEMS.get(category));
-                    break;
-                }
+        Vault.getEconomy().withdrawPlayer(p,PAYMENT);
+
+        ItemStack bingoItem=null;
+        int bingoNum = (int) (Math.random() * categoryRandomMax);
+        String category=null;
+        for(int i : PROBABILITY.keySet()){
+            if(bingoNum < i){
+                category=PROBABILITY.get(i);
+                if(!category.equals("miss"))bingoItem=(ItemStack) Util.getRandomIndex(CATEGORY_CONTAIN_ITEMS.get(category));
+                break;
             }
+        }
+        int nowStock = getAndUpdateStock(true, !category.equals("miss"));
+
+        final String category_f = category;
+        final ItemStack bingoItem_f = bingoItem;
+        Bukkit.getScheduler().runTaskAsynchronously(plugin,()->{;
+        ItemStack memo=null;
 
             int fin=0;
             for(int i = 1; i<=SPIN_TIME[ITEM_FRAME_COUNT -1]; i++){
-               for(int j = fin; j< ITEM_FRAME_COUNT; i++) Util.setItemFrame(ITEM_FRAMES.get(j), DISPLAY_ITEMS.get((int)(Math.random() * DISPLAY_ITEMS.size())));
+               for(int j = fin; j< ITEM_FRAME_COUNT; j++){
+                   ITEM_FRAMES.get(j).setItem(DISPLAY_ITEMS.get((int)(Math.random() * DISPLAY_ITEMS.size())));}
+
                if(i==SPIN_TIME[0]){
                    fin++;
-                   if(!category.equals("miss"))Util.setItemFrame(ITEM_FRAMES.get(0),bingoItem);
+                   if(!category_f.equals("miss"))ITEM_FRAMES.get(0).setItem(bingoItem_f);
                }
                else if(i==SPIN_TIME[1]){
                    fin++;
-                   if(!category.equals("miss"))Util.setItemFrame(ITEM_FRAMES.get(1),bingoItem);
-                   else memo = Util.getItemFrame(ITEM_FRAMES.get(1));
+                   if(!category_f.equals("miss"))ITEM_FRAMES.get(1).setItem(bingoItem_f);
+                   else memo = ITEM_FRAMES.get(1).getItem();
                }else if(i==SPIN_TIME[2]){
-                   if(!category.equals("miss"))Util.setItemFrame(ITEM_FRAMES.get(2),bingoItem);
+                   if(!category_f.equals("miss"))ITEM_FRAMES.get(2).setItem(bingoItem_f);
                    else{
                        List<ItemStack> dispItems = new ArrayList<>(DISPLAY_ITEMS);
                        dispItems.remove(memo);
-                       Util.setItemFrame(ITEM_FRAMES.get(2),dispItems.get((int)(Math.random() * dispItems.size())));
+                       ITEM_FRAMES.get(2).setItem(dispItems.get((int)(Math.random() * dispItems.size())));
                    }
                }
                 try {
-                    Thread.sleep(50*5);
+                    Thread.sleep(50*SPIN_TICK);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
 
-            if(category.equals("miss"))Util.sendPrefixMessage(p,"§c§lはずれました...");
+            if(category_f.equals("miss"))Util.sendPrefixMessage(p,"§c§lはずれました...");
             else{
-                int wonMoney = CONSTANT_MONEY_BY_CATEGORY.get(category);
-                if(wonMoney==0)wonMoney=(int)(nowStock * MULTIPLIER_BY_CATEGORY.get(category));
-                String bingoType = (bingoItem.getItemMeta().hasDisplayName() ? bingoItem.getItemMeta().getDisplayName() : bingoItem.getType().toString());
+                int wonMoney = CONSTANT_MONEY_BY_CATEGORY.get(category_f);
+                if(wonMoney==0)wonMoney=(int)(nowStock * MULTIPLIER_BY_CATEGORY.get(category_f));
+                String bingoType = (bingoItem_f.getItemMeta().hasDisplayName() ? bingoItem_f.getItemMeta().getDisplayName() : bingoItem_f.getType().toString());
                 Util.sendPrefixMessage(p,"§a§lおめでとうございます！！§d§l『"+bingoType+"』§a§l揃いです！");
                 Vault.getEconomy().depositPlayer(p,wonMoney);
-                if(GAVE_ITEM_BY_CATEGORY.containsKey(category))p.getInventory().addItem(GAVE_ITEM_BY_CATEGORY.get(category));
-                if(BROADCAST.contains(category))Util.broadcast("§b§l"+p.getName()+"§a§lが§d§l『"+NAME+"』§a§lで、§6§l"+bingoType+"揃い§a§lにより§e§l"+wonMoney+"円§a§l獲得！！");
-                if(TITLE.contains(category))Util.title("§c§l"+NAME+"§6§lで当選！","§b§l"+p.getName()+"§a§lが§e§l"+wonMoney+"円§a§l獲得！！");
+                if(GAVE_ITEM_BY_CATEGORY.containsKey(category_f))p.getInventory().addItem(GAVE_ITEM_BY_CATEGORY.get(category_f));
+                if(BROADCAST.contains(category_f))Util.broadcast("§b§l"+p.getName()+"§a§lが§d§l『"+NAME+"』§a§lで、§6§l"+bingoType+"揃い§a§lにより§e§l"+wonMoney+"円§a§l獲得！！");
+                if(TITLE.contains(category_f))Util.title("§c§l"+NAME+"§6§lで当選！","§b§l"+p.getName()+"§a§lが§e§l"+wonMoney+"円§a§l獲得！！");
                 SYSTEM_YML.set("stock",SYSTEM_YML.getInt("def_stock",0));
-                getAndUpdateStock(false);
                 CustomConfig.saveYmlByID(ID);
-                setMode(NEXT_MODE.get(category));
+                setMode(NEXT_MODE.get(category_f));
             }
             isSpinning = false;
         });
@@ -129,17 +141,16 @@ public class Spin {
         Slot.GAVE_ITEM_BY_CATEGORY.clear();
         Slot.CATEGORY_CONTAIN_ITEMS.clear();
         Slot.BROADCAST.clear();Slot.TITLE.clear();
-
         YamlConfiguration yml = CustomConfig.getYmlByID(Slot.ID,mode);
         AtomicInteger now = new AtomicInteger(0);
-        yml.getKeys(false).forEach(e->{
+        yml.getKeys(false).stream().filter(g->!g.equals("miss")).forEach(e->{
             int weight = yml.getInt(e+".weight");
             if(weight == 0)return;
             Slot.PROBABILITY.put(now.addAndGet(weight),e);
-            int size = yml.getConfigurationSection(e).getKeys(false).size();
+            int size = yml.getConfigurationSection(e+".display").getKeys(false).size();
             ItemStack[] itemStacks = new ItemStack[size];
             for(int i = 0;i<size;i++){
-                ItemStack item = yml.getItemStack(e+"."+i);
+                ItemStack item = yml.getItemStack(e+".display."+i);
                 if(item == null){
                     if(i==0)return;
                     else break;
@@ -155,18 +166,22 @@ public class Spin {
             Slot.CATEGORY_CONTAIN_ITEMS.put(e,itemStacks);
             Slot.NEXT_MODE.put(e,yml.getString(e+".nextmode"));
         });
-        Slot.PROBABILITY.put(now.addAndGet(yml.getInt("miss.weight")),"miss");
+        Slot.PROBABILITY.put(now.addAndGet(yml.getInt("miss.weight",1)),"miss");
         Slot.categoryRandomMax = now.get();
     }
 
 
-    public int getAndUpdateStock(boolean toAdd){
+    public int getAndUpdateStock(boolean toAdd,boolean isBingo){
         int nowStock = SYSTEM_YML.getInt("stock",SYSTEM_YML.getInt("def_stock",0)) + (toAdd ? STOCK_PER_SPIN : 0);
         SYSTEM_YML.set("stock",nowStock);
         CustomConfig.saveYmlByID(ID);
 
-        MAIN_SYSTEM.getSignSystem().update(SIGN_POS,nowStock);
-
+        MAIN_SYSTEM.getSignSystem().update(SIGN,nowStock);
+        if(isBingo){
+            Bukkit.getScheduler().runTaskLater(plugin,()->{
+                MAIN_SYSTEM.getSignSystem().update(SIGN,0);
+            },SPIN_TIME[ITEM_FRAME_COUNT -1] * SPIN_TICK);
+        }
 
         return nowStock;
     }
