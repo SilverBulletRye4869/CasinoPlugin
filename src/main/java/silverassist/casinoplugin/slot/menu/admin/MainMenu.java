681eb0dde7deb8ed26c96125588b3bf73ebd91f0
SilverBullet_Rye <silverbulletrye4869@gmail.com>
2023-03-17 11:04:33 +0900

    現在の状態を表示するように変更

--- src/main/java/silverassist/casinoplugin/slot/menu/admin/MainMenu.java
+++ src/main/java/silverassist/casinoplugin/slot/menu/admin/MainMenu.java
@@ -2,6 +2,7 @@ package silverassist.casinoplugin.slot.menu.admin;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -12,9 +13,11 @@ import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.plugin.java.JavaPlugin;
+import silverassist.casinoplugin.CustomConfig;
 import silverassist.casinoplugin.Util;
 import silverassist.casinoplugin.slot.MainSystem_slot;
 
+import java.util.List;
 import java.util.Map;
 
 public class MainMenu {
@@ -22,25 +25,28 @@ public class MainMenu {
     private final MainSystem_slot MAIN_SYSTEM;
     private final Player P;
     private final String ID;
+    private final YamlConfiguration SYSTEM_YML;
 
     public MainMenu(JavaPlugin plugin,MainSystem_slot mainSystem_slot, Player p,String id){
         this.plugin = plugin;
         this.MAIN_SYSTEM = mainSystem_slot;
         this.P = p;
         this.ID = id;
+        this.SYSTEM_YML = CustomConfig.getYmlByID(id);
+
         plugin.getServer().getPluginManager().registerEvents(new listener(),plugin);
     }
 
     public void open(){
         Inventory inv = Bukkit.createInventory(P,45, Util.PREFIX+"§d§l"+ID+"§a§lの編集");
         Util.invFill(inv);
-        inv.setItem(10,Util.createItem(Material.NAME_TAG,"§f§lスロット名を編集"));
-        inv.setItem(12,Util.createItem(Material.GOLD_INGOT,"§e§lスロット料金を編集"));
+        inv.setItem(10,Util.createItem(Material.NAME_TAG,"§f§lスロット名を編集", List.of("§f現在: "+SYSTEM_YML.getString("name"))));
+        inv.setItem(12,Util.createItem(Material.GOLD_INGOT,"§e§lスロット料金を編集", List.of("§f現在: "+SYSTEM_YML.getString("payment")+"円")));
         inv.setItem(14,Util.createItem(Material.CHEST,"§6§lスロットの目を編集"));
         inv.setItem(16,Util.createItem(Material.STICK,"§c§l額縁を設定する棒を取得",null, Map.of(Enchantment.DAMAGE_ALL,1)));
-        inv.setItem(28,Util.createItem(Material.CLOCK,"§e§l回転時間を調整"));
-        inv.setItem(30,Util.createItem(Material.DIAMOND_BLOCK,"§b§l初期ストックを変更"));
-        inv.setItem(32,Util.createItem(Material.EMERALD_BLOCK,"§b§l回転毎のストックを変更"));
+        inv.setItem(28,Util.createItem(Material.CLOCK,"§e§l回転時間を調整",List.of("§f§l1つめが揃うまでの時間: "+SYSTEM_YML.getString("spintime.1"),"§f§l1つめが揃って2つ目が揃うまでの時間: "+SYSTEM_YML.getString("spintime.2"),"§f§l2つめが揃って3つ目が揃うまでの時間: "+SYSTEM_YML.getString("spintime.3"))));
+        inv.setItem(30,Util.createItem(Material.DIAMOND_BLOCK,"§b§l初期ストックを変更",List.of("§f現在: "+SYSTEM_YML.getString("def_stock")+"円")));
+        inv.setItem(32,Util.createItem(Material.EMERALD_BLOCK,"§b§l回転毎のストックを変更",List.of("§f現在: "+SYSTEM_YML.getString("stock_per_spin")+"円")));
         inv.setItem(34,Util.createItem(Material.COMPASS,"§a§lスロットをリロード"));
         Util.delayInvOpen(P,inv);
     }
